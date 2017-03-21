f36ece888e3b145ef95044827312c15f24fec337
unformatt <unformatt@gmail.com>
2017-03-21 14:01:50 -0300

    Bugfix on require

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## v0.1.0
 
 * Initial release
+
+## v0.2.0
+
+* Bugfix error
--- lib/capistrano/unformatt/monit.rb
+++ lib/capistrano/unformatt/monit.rb
@@ -1,1 +1,1 @@
-load File.expand_path("../../tasks/rake.rake", __FILE__)
+load File.expand_path("../../tasks/monit.rake", __FILE__)
